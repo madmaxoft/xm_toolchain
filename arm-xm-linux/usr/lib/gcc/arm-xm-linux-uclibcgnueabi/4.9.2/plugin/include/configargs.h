914e9501b5d31f80a06d857c4d3dbe071d8725c5
heyan <heyan3502@163.com>
2022-06-21 15:45:09 +0800

    init toolchain

---
+++ arm-xm-linux/usr/lib/gcc/arm-xm-linux-uclibcgnueabi/4.9.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --prefix=/opt/xm_toolchain/arm-xm-linux/usr --sysconfdir=/opt/xm_toolchain/arm-xm-linux/etc --enable-static --target=arm-xm-linux-uclibcgnueabi --with-sysroot=/opt/xm_toolchain/arm-xm-linux/usr/arm-xm-linux-uclibcgnueabi/sysroot --disable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --with-gmp=/opt/xm_toolchain/arm-xm-linux/usr --with-mpfr=/opt/xm_toolchain/arm-xm-linux/usr --enable-target-optspace --disable-libsanitizer --enable-tls --disable-libmudflap --enable-threads --with-mpc=/opt/xm_toolchain/arm-xm-linux/usr --without-isl --without-cloog --disable-decimal-float --with-arch=armv7-a --with-abi=aapcs-linux --with-cpu=cortex-a5 --with-fpu=neon-vfpv4 --with-float=softfp --with-mode=arm --with-pkgversion='Buildroot 2014.08' --with-bugurl=http://bugs.buildroot.net/ --enable-languages=c,c++ --with-build-time-tools=/opt/xm_toolchain/arm-xm-linux/usr/arm-xm-linux-uclibcgnueabi/bin --enable-shared --disable-libgomp";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "aapcs-linux" }, { "cpu", "cortex-a5" }, { "arch", "armv7-a" }, { "float", "softfp" }, { "mode", "arm" }, { "fpu", "neon-vfpv4" }, { "tls", "gnu" } };
